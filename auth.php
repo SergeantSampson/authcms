359adc1c603eb091bbb3968bed5deb6bed719646
SergeantSampson <707761@gmail.com>
2012-12-18 07:55:03 -0700

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,17 +1,17 @@
 <?php
-
-
+#includes files
 include 'config.php';
 
 if(isset($host)){
-if(isset($user)){
-if(isset($password)){
-if(isset($dbname)){
-if(isset($table)){
+  if(isset($user)){
+    if(isset($password)){
+      if(isset($dbname)){
+        if(isset($table)){
 $auth == 1;
+        }
+      }
+    }
+  }
 }
-}
-}
-}
-}
+//end all if statements 
 ?>
